7b53444452a31157c8bcbd91b50167d9ce03d613
Garden <gardendistrict0x0@outlook.com>
2025-06-21 11:05:42 +1000

    implement proc_macro_derive for Crap (wip still)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Crap, attributes(crap))]
+pub fn derive_crap(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // this is just debug for now
+    println!("Deriving Crap for: {}", input.ident);
+
 }
